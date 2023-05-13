b4ab63fcf92ee0aaf5784fbcbfac4daa3c8270ff
E-Wekesi <127873642+E-Wekesi@users.noreply.github.com>
2023-05-13 14:59:51 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+let tablinks= document.getElementsByClassName('tab-links');
+let tabcontents= document.getElementsByClassName('tab-contents');
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove('active-link')
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove('active-tab')
+    }
+    event.currentTarget.classList.add('active-link')
+    document.getElementById(tabname).classList.add('active-tab')
+}
+
+let sidemenu = document.getElementById('sidemenu')
+function openmenu(){
+    sidemenu.style.right=0;
+}
+function closemenu(){
+    sidemenu.style.right="-200px";
+}
+const scriptURL = 'https://script.google.com/macros/s/AKfycbz6YCTHDdBBkMvf-2bbWIb-Iy-x5teoofuJhU6DWtHx1qkASjPRpVR7OrxYRUXns-gsIg/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response =>{
+        msg.innerHTML = 'Message Sent Successfully'
+        setTimeout(function(){
+            msg.innerHTML = ''
+        },5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
