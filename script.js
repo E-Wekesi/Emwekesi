f1887f9c47735acd8ff45fd87a6de2f4f23718aa
E-Wekesi <127873642+E-Wekesi@users.noreply.github.com>
2023-08-08 11:56:44 +0300

    change links

--- script.js
+++ script.js
@@ -19,19 +19,3 @@ function openmenu(){
 function closemenu(){
     sidemenu.style.right="-200px";
 }
-const scriptURL = 'https://script.google.com/macros/s/AKfycbz6YCTHDdBBkMvf-2bbWIb-Iy-x5teoofuJhU6DWtHx1qkASjPRpVR7OrxYRUXns-gsIg/exec'
-  const form = document.forms['submit-to-google-sheet']
-  const msg = document.getElementById("msg");
-
-  form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response =>{
-        msg.innerHTML = 'Message Sent Successfully'
-        setTimeout(function(){
-            msg.innerHTML = ''
-        },5000)
-        form.reset()
-      })
-      .catch(error => console.error('Error!', error.message))
-  })
